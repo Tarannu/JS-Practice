552252b583abab2bd1d1c6813037227904c9015b
Tarannu <tarannum.aiub@gmail.com>
2020-10-01 23:09:19 -0400

    adding JS and form features

--- html css/js prsctice 1/index.js
+++ html css/js prsctice 1/index.js
@@ -1,4 +1,65 @@
-function myFunction() {
-    var x = document.getElementById("frm1");
-    document.getElementById("demo").innerHTML = x;
+//selectors
+const todoInput=document.querySelector('.todo-input')
+const todoButton=document.querySelector('.todo-button')
+const todoList=document.querySelector('.todo-list')
+const filterOption=document.querySelector('.filter-todo');
+//event listeners
+todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click',filterTodo);
+
+//functions
+function addTodo(event){
+  //prevent form from submitting
+  event.preventDefault();
+  //Todo Div
+  const todoDiv=document.createElement('div');
+  todoDiv.classList.add('todo');
+  //create LI
+  const newTodo=document.createElement('li');
+  newTodo.innerText=todoInput.value; //adding input to your list
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  //check button
+  const completedButton=document.createElement('button');
+  completedButton.innerHTML='<i class="fas fa-check></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //trash button
+  //check button
+  const trashButton=document.createElement('button');
+  trashButton.innerHTML='<i class="fas fa-trash></i>';
+  trashButton.classList.add("complete-btn");
+  todoDiv.appendChild(trashButton);
+  //append to list
+  todoList.appendChild(todoDiv);
+  //clear to do input value
+  todoInput.value="";
+
+}
+function deleteCheck(e){
+  const item=e.target;
+  //delete todo
+  if(item.classList[0]==="trash-btn"){
+    const todo=item.parentElement;
+    todo.remove();
   }
+
+}
+function filterTodo(e){
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo){
+    switch(e.target.value){
+      case "all":
+        break;
+      case "completed":
+        if(todo.classList.contains('completed')){
+          todo.style.display='flex';
+        }
+        else{
+          todo.style.display="none";
+        }
+    }
+  });
+
+}
