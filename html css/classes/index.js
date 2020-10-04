0f303b4687a124ed27c7444b92615c16db2827b5
Tarannu <tarannum.aiub@gmail.com>
2020-10-04 02:07:12 -0400

    new commit added to add a object to JS

--- html css/classes/index.js
+++ html css/classes/index.js
@@ -1,20 +1,52 @@
 //selectors
-var weightInput1=document.getElementsByClassName('.weightinput1').value;
-var heightInput1=document.getElementsByClassName('.heightinput1').value;
-var weightInput2=document.getElementsByClassName('.weightinput2').value;
-var heightInput2=document.getElementsByClassName('.heightinput2').value;
+var weightInput1=document.getElementsByClassName('.weightinput1');
+var heightInput1=document.getElementsByClassName('.heightinput1');
+var weightInput2=document.getElementsByClassName('.weightinput2');
+var heightInput2=document.getElementsByClassName('.heightinput2');
 const mybutton=document.getElementById("myBtn");
-
 
 //event selector
 mybutton.addEventListener('click',printOutput);
 
 //function
 function printOutput(e){
+    
+    let divider1=Math.pow(heightInput1.value,2);
+    let result1=(weightInput1.value/divider1);
+    let bmi1=result1.toString();
 
-    let divider1=Math.pow(heightInput1,2)
-    let bmi1=(weightInput1/divider1);
-    console.log("The Bmi is ",Math.round(bmi1));
+    console.log("The Bmi of person 1 is ",bmi1);
 }
 
-//console.log("the output type is ",typeof(bmi1));
+var personInformation={
+    name: 'Bebo',
+    age:20,
+    educattion:['Bachelors',' IIT',' 2020' ],
+    address:{
+        presentAddress: {
+            street:'8960 nw 8th st',
+            city: 'Miami',
+        },
+        permanentAddress:{
+            street:'10317 NW 7th st',
+            city:'Orlando',
+        },
+
+    },
+    skills: {
+        technical:['c++','Java','JavaScript','HTML','CSS'],
+        soft:['MS VISO','Visual Code','R Studio'],
+    },
+    experience:{
+        companyName:'Microsoft',
+        role:'Software Engineer',
+        activeYear:2015-2018,
+        currentlyWorking:false,
+    },
+};
+console.log("The HW 1 question 2 answer is as follows click on the button for question 1 will be shoed from 3rd output")
+console.log("Hi, my name is",personInformation.name, "I am", personInformation.age," years old. My present address is", personInformation.address.presentAddress.street,personInformation.address.presentAddress.city,
+" I took my",personInformation.educattion[0]," from", personInformation.educattion[1],". I am currently working in ",
+ personInformation.experience.companyName," as a",personInformation.experience.role,".");
+
+
